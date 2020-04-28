f679987448c368ef1ee4dfc5de4490c0eeccdd29
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2020-04-27 22:16:07 -0300

    Updates

---
+++ precache-manifest.468c34bccd1f93c086c9cf7b788116ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59e7f7a3b7020e1433ac941754ee4d8",
+    "url": "/superhero/index.html"
+  },
+  {
+    "revision": "61d59ddf267a1faddd89",
+    "url": "/superhero/static/js/2.8ce34fa3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/superhero/static/js/2.8ce34fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f9b89610c91351e02a",
+    "url": "/superhero/static/js/main.00d782d1.chunk.js"
+  },
+  {
+    "revision": "9b2c2e2dbf5c8f88d09d",
+    "url": "/superhero/static/js/runtime-main.f197a5bf.js"
+  }
+]);
