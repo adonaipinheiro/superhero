53d208c726bcf3916acf301116c31e86f8955f65
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2020-04-28 09:50:44 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Heroes from '../pages/Heroes';
-// import PageNotFound from '../pages/PageNotFound';
+import PageNotFound from '../pages/PageNotFound';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/heroes/:id" exact component={Heroes} />
-    {/* <Route component={PageNotFound} /> */}
+    <Route path="/heroes/:id" component={Heroes} />
+    <Route component={PageNotFound} />
   </Switch>
 );
 
