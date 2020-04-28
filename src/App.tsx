0a791f20b8f985df76111dcd4bac1f10aa9d103d
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2020-04-28 09:53:24 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <BrowserRouter>
+    <HashRouter>
       <Routes />
-    </BrowserRouter>
+    </HashRouter>
     <GlobalStyle />
   </>
 );
